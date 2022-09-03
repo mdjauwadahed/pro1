fe554d21f0d217c41f773db04f4970964e749915
mdjauwadahed <112750458+mdjauwadahed@users.noreply.github.com>
2022-09-03 23:53:06 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,30 @@
+function myfunction(){
+    window.location.href="next.html"
+}
+function add(x, y,) {
+ var x1 = parseInt(document.getElementById(x).value)
+ var x2 = parseInt(document.getElementById(y).value)
+ var calculations = x1+x2 ;
+ alert(calculations)
+}
+function sub(x, y,) {
+    var x1 = parseInt(document.getElementById(x).value)
+    var x2= parseInt(document.getElementById(y).value)
+    
+    var calculations = x1-x2 ;
+    alert(calculations)
+   }
+   function mul(x, y,) {
+    var x1 = parseInt(document.getElementById(x).value)
+    var x2= parseInt(document.getElementById(y).value)
+    
+    var calculations = x1 * x2 ;
+    alert(calculations)
+   }
+   function div(x, y,) {
+    var x1 = parseInt(document.getElementById(x).value)
+    var x2 = parseInt(document.getElementById(y).value)
+    
+    var calculations = x1 /x2 ;
+    alert(calculations)
+   }
